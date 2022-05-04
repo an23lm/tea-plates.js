a96538bbfe64bab77298a16df9075da2d2ae6acf
Anselm Joseph <anselmjs@icloud.com>
2022-05-04 19:56:02 +0530

    address fix

--- Demo/data.js
+++ Demo/data.js
@@ -1,40 +1,41 @@
-jsondata = [{
-        "first-name": "Spongebob",
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name",
-        "notification-count": 2
-    },
-    {
-        "first-name": "Spongebob",
+jsondata = [
+	{
+		"first-name": "Spongebob",
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+		"notification-count": 2,
+	},
+	{
+		"first-name": "Spongebob",
 
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name"
-    },
-    {
-        "first-name": "Spongebob",
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name",
-        "notification-count": 1
-    },
-    {
-        "first-name": "Spongebob",
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name"
-    },
-    {
-        "first-name": "Spongebob",
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name"
-    },
-    {
-        "first-name": "Spongebob",
-        "last-name": "Squarepants",
-        "jid": "21345",
-        "address": "420, Street Name"
-    }
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+	},
+	{
+		"first-name": "Spongebob",
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+		"notification-count": 1,
+	},
+	{
+		"first-name": "Spongebob",
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+	},
+	{
+		"first-name": "Spongebob",
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+	},
+	{
+		"first-name": "Spongebob",
+		"last-name": "Squarepants",
+		jid: "21345",
+		address: "lives under a pineapple",
+	},
 ];
