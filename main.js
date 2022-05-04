67e22708ad86115a529160bcb1dc6b3ea3d066e7
Anselm Joseph <anselmjs@icloud.com>
2022-05-04 19:44:58 +0530

    updated demo

--- main.js
+++ main.js
@@ -1,92 +1,123 @@
 function setup() {
-    let loadingTemplate =
-        `<div class="poc-loading-block">
+	/* tea plate reusable card components */
+	let loadingTemplate = `<div class="poc-loading-block">
             <div class="loading-address"></div>
         </div>`;
 
-    let template = (data) => {
-        let ts = `<div class="poc-block">`
-        if (data['notification-count'] != undefined) {
-            ts += `<div class="notification">${data['notification-count']}</div>`
-        }
-        ts += `<div class="first-name poc-text" id="${data['jid']}">
-                ${data['first-name']} ${data['last-name']}
+	let template = (data) => {
+		let ts = `<div class="poc-block">`;
+		if (data["notification-count"] != undefined) {
+			ts += `<div class="notification">${data["notification-count"]}</div>`;
+		}
+		ts += `<div class="first-name poc-text" id="${data["jid"]}">
+                ${data["first-name"]} ${data["last-name"]}
             </div>
-            <div class="address poc-text">${data['address']}</div>
+            <div class="address poc-text">${data["address"]}</div>
         </div>`;
 
-        return ts;
-    }
+		return ts;
+	};
 
-    function onclick(event, data) {
-        alert('clicked');
-        console.log(data);
-    }
+	let noData = (() => {
+		let ts = `<div class="poc-block">`;
+		ts += `<div class="first-name poc-text">
+                No Data
+            </div>
+        </div>`;
 
-    let noData = `<div>No Data available</div>`
-    var newTemplate1 = new TeaPlates('poc-wrapper-1', template, loadingTemplate, noData);
-    newTemplate1.registerEventListeners('click', onclick);
-    newTemplate1.showLoading(3);
+		return ts;
+	})();
 
-    function newDS() {
-        newTemplate1.setData(jsondata);
-        newTemplate1.insertObjects(() => {
-            newTemplate1.updateDataForUid(1, (data) => {
-                data['notification-count'] = 10;
-                return data;
-            });
-            newTemplate1.reloadObjectAtUid(1);
-        });
-    }
+	function onclick(event, data) {
+		alert(
+			"Woah woah! I've only spend 15 mins making this demo! Feel free to modify this to add more Spongebob Trivia!"
+		);
+		console.log(data);
+	}
 
-    setTimeout(() => {
-        newDS();
-        // newTemplate1.removeAllObjects();
-    }, 5000);
-    
-    setTimeout(() => {
-        newTemplate1.showLoading(3);
-    }, 10000);
+	/* template 1 - left most column */
+	var newTemplate1 = new TeaPlates(
+		"poc-wrapper-1",
+		template,
+		loadingTemplate,
+		noData
+	);
+	newTemplate1.registerEventListeners("click", onclick);
+	newTemplate1.showLoading(3);
 
-    // var newTemplate2 = new TeaPlates('poc-wrapper-2', template, loadingTemplate);
-    // newTemplate2.showLoading(3);
-    // setTimeout(() => {
-    //     insertData(newTemplate2);
-    // }, 9000)
+	function newDataSet() {
+		newTemplate1.setData(jsondata);
 
-    // setTimeout(() => {
-    //     insertData(newTemplate1, () => {
-    //         var obj = newTemplate1.removeObjectWithUID(2);
-    //         newTemplate2.setData(obj);
-    //         newTemplate2.insertObjects();
-            
-    //     });
-    // }, 5000)
+		newTemplate1.insertObjects(() => {
+			updateNotificationCount();
+		});
+	}
 
-    // var newTemplate3 = new TeaPlates('poc-wrapper-3', template, loadingTemplate);
-    // newTemplate3.showLoading(4);
-    // setTimeout(() => {
-    //     insertData(newTemplate3);
-    // }, 10000)
+	// Dynamic update dataset values
+	function updateNotificationCount() {
+		setTimeout(() => {
+			newTemplate1.updateDataForUid(1, (data) => {
+				data["notification-count"] = 10;
+				data["address"] = "Dynamically updated value!";
+				return data;
+			});
+			newTemplate1.reloadObjectAtUid(1);
+		}, 2000);
+	}
 
-    // var newTemplate4 = new TeaPlates('poc-wrapper-4', template, loadingTemplate);
-    // newTemplate4.showLoading(2);
-    // setTimeout(() => {
-    //     insertData(newTemplate4);
-    //     setTimeout(() => {
-    //         newTemplate4.updateDataForUid(1, (data) => {
-    //             data['notification-count'] = 10;
-    //             return data;
-    //         });
-    //         newTemplate4.reloadObjectAtUid(1);
-    //     }, 2000);
-    // }, 3000)
+	setTimeout(() => {
+		newDataSet();
+		// newTemplate1.removeAllObjects();
+	}, 2000);
 
-    // var newTemplate5 = new TeaPlates('poc-wrapper-5', template, loadingTemplate);
-    // newTemplate5.showLoading(5);
-    // setTimeout(() => {
-    //     insertData(newTemplate5);
-    // }, 12000)
+	/* template 2 */
+	var newTemplate2 = new TeaPlates(
+		"poc-wrapper-2",
+		template,
+		loadingTemplate,
+		noData
+	);
+	newTemplate2.showLoading(3);
+	setTimeout(() => {
+		newTemplate2.hideLoading();
+		newTemplate2.showNoDataElement();
+	}, 3000);
+
+	var newTemplate3 = new TeaPlates(
+		"poc-wrapper-3",
+		template,
+		loadingTemplate,
+		noData
+	);
+	newTemplate3.showLoading(4);
+	setTimeout(() => {
+		newTemplate3.hideLoading();
+		newTemplate3.showNoDataElement();
+	}, 4000);
+
+	var newTemplate4 = new TeaPlates(
+		"poc-wrapper-4",
+		template,
+		loadingTemplate,
+		noData
+	);
+	newTemplate4.showLoading(2);
+	setTimeout(() => {
+		newTemplate4.hideLoading();
+		newTemplate4.showNoDataElement();
+	}, 3000);
+
+	var newTemplate5 = new TeaPlates(
+		"poc-wrapper-5",
+		template,
+		loadingTemplate,
+		noData
+	);
+	newTemplate5.showLoading(5);
+	setTimeout(() => {
+		newTemplate5.hideLoading();
+		newTemplate5.showNoDataElement();
+	}, 3500);
 }
 
 window.onload = setup;
